bc4986c8df2c25f53e147620a9df3043cde13ad0
zakzinzan123 <zakzinzan123@gmail.com>
2022-10-13 01:35:10 +0800

    Add function for basic game logic

--- script.js
+++ script.js
@@ -19,3 +19,47 @@ function capitalize(string) {
 
     return firstLetter + subString;
 }
+
+function playRound(playerSelection, computerSelection) {
+    const computerString = `Computer chooses ${computerSelection}!\n`;
+
+    playerSelection = capitalize(playerSelection);
+
+    if (playerSelection === computerSelection) {
+        return `${computerString}It's a draw!\nYour Score: ${playerScore}  Computer's Score: ${computerScore}`;
+    } else if (playerSelection === "Rock") {
+
+        if (computerSelection === "Scissors") {
+            playerScore++;
+            console.log(playerScore + " " + computerScore);
+            return `${computerString}You WIN! Rock beats Scissors!\nYour Score: ${playerScore}  Computer's Score: ${computerScore}`;
+        } else {
+            computerScore++;
+            console.log(playerScore + " " + computerScore);
+            return `${computerString}You LOSE! Paper beats Rock!\nYour Score: ${playerScore}  Computer's Score: ${computerScore}`;
+        }
+
+    } else if (playerSelection === "Paper") {
+
+        if (computerSelection === "Rock") {
+            playerScore++;
+            return `${computerString}You WIN! Paper beats Rock!\nYour Score: ${playerScore}  Computer's Score: ${computerScore}`
+        } else {
+            computerScore++;
+            return `${computerString}You LOSE! Scissors beats Paper!\nYour Score: ${playerScore}  Computer's Score: ${computerScore}`;
+        }
+
+    } else if (playerSelection === "Scissors") {
+
+        if (computerSelection === "Rock") {
+            computerScore++;
+            return `${computerString}You LOSE! Rock beats Scissors!\nYour Score: ${playerScore}  Computer's Score: ${computerScore}`;
+        } else {
+            playerScore++;
+            return `${computerString}You WIN! Scissors beats Paper!\nYour Score: ${playerScore}  Computer's Score: ${computerScore}`;
+        }
+
+    } else {
+        return "I don't recognize that choice";
+    }
+}
