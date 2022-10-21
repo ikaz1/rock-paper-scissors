25d65ae0167e0d8c67eba73ca1f69f0f1f934829
zakzinzan123 <zakzinzan123@gmail.com>
2022-10-22 00:27:53 +0800

    Add basic HTML structure and change JS code

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let playerScore = 0;
 let computerScore = 0;
 
+const buttons = document.querySelectorAll('button');
+buttons.forEach(btn => {
+    btn.addEventListener('click', playRound);
+});
+
 function getComputerChoice() {
     let randomChoice = Math.floor(Math.random() * 3) + 1;
     
@@ -13,72 +18,46 @@ function getComputerChoice() {
     }
 }
 
-function capitalize(string) {
-    const firstLetter = string[0].toUpperCase();
-    const subString = string.substring(1).toLowerCase();
+function playRound(e) {
+    const computerChoice = getComputerChoice();
+    const playerChoice = e.target.innerText;
 
-    return firstLetter + subString;
+
+    // if (playerSelection === computerSelection) {
+    //     //DRAW
+    // } else if (playerSelection === "Rock") {
+
+    //     if (computerSelection === "Scissors") {
+    //         playerScore++;
+    //         console.log(playerScore + " " + computerScore);
+    //         // WIN rock beats scissors
+    //     } else {
+    //         computerScore++;
+    //         console.log(playerScore + " " + computerScore);
+    //         // LOSE paper beats rock
+    //     }
+
+    // } else if (playerSelection === "Paper") {
+
+    //     if (computerSelection === "Rock") {
+    //         playerScore++;
+    //         // WIN paper beats rock
+    //     } else {
+    //         computerScore++;
+    //         // LOSE scissors beats paper
+    //     }
+
+    // } else if (playerSelection === "Scissors") {
+
+    //     if (computerSelection === "Rock") {
+    //         computerScore++;
+    //         // LOSE rock beats scissors
+    //     } else {
+    //         playerScore++;
+    //         // WIN scissors beats paper
+    //     }
+
+    // } else {
+    //     // choice not recognized
+    // }
 }
-
-function playRound(playerSelection, computerSelection) {
-    const computerString = `Computer chooses ${computerSelection}!\n`;
-
-    playerSelection = capitalize(playerSelection);
-
-    if (playerSelection === computerSelection) {
-        return `${computerString}It's a draw!\nYour Score: ${playerScore}  Computer's Score: ${computerScore}`;
-    } else if (playerSelection === "Rock") {
-
-        if (computerSelection === "Scissors") {
-            playerScore++;
-            console.log(playerScore + " " + computerScore);
-            return `${computerString}You WIN! Rock beats Scissors!\nYour Score: ${playerScore}  Computer's Score: ${computerScore}`;
-        } else {
-            computerScore++;
-            console.log(playerScore + " " + computerScore);
-            return `${computerString}You LOSE! Paper beats Rock!\nYour Score: ${playerScore}  Computer's Score: ${computerScore}`;
-        }
-
-    } else if (playerSelection === "Paper") {
-
-        if (computerSelection === "Rock") {
-            playerScore++;
-            return `${computerString}You WIN! Paper beats Rock!\nYour Score: ${playerScore}  Computer's Score: ${computerScore}`
-        } else {
-            computerScore++;
-            return `${computerString}You LOSE! Scissors beats Paper!\nYour Score: ${playerScore}  Computer's Score: ${computerScore}`;
-        }
-
-    } else if (playerSelection === "Scissors") {
-
-        if (computerSelection === "Rock") {
-            computerScore++;
-            return `${computerString}You LOSE! Rock beats Scissors!\nYour Score: ${playerScore}  Computer's Score: ${computerScore}`;
-        } else {
-            playerScore++;
-            return `${computerString}You WIN! Scissors beats Paper!\nYour Score: ${playerScore}  Computer's Score: ${computerScore}`;
-        }
-
-    } else {
-        return "I don't recognize that choice";
-    }
-}
-
-function game() {
-    for (let i = 0; i < 5; i++) {
-        const playerChoice = prompt("There are 5 rounds! Choose either Rock, Paper, or Scissors!");
-        const computerChoice = getComputerChoice();
-
-        alert(playRound(playerChoice, computerChoice));
-    }
-
-    if (playerScore > computerScore) {
-        alert("You WIN the game!");
-    } else if (playerScore < computerScore) {
-        alert("You LOSE the game!")
-    } else {
-        alert("The game is TIED!");
-    }
-}
-
-game();
