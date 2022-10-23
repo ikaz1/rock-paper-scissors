109efef27b9605be3a43ca8501db09e78ad284bd
zakzinzan123 <zakzinzan123@gmail.com>
2022-10-23 23:30:45 +0800

    Add icons and additional styling

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const buttonChoice = document.querySelectorAll('.player-choice');
+const playerOptions = document.querySelectorAll('.option');
 const buttonPlayAgain = document.querySelector('#btn-play-again');
 const playerResult = document.querySelector('#player-score');
-const computerResult = document.querySelector('#computer-score');
+const botResult = document.querySelector('#bot-score');
 const results = document.querySelector('#results');
 const choiceDisplay = document.querySelector('#choice-display');
 let playerScore = 0;
-let computerScore = 0;
+let botScore = 0;
 
-function getComputerChoice() {
+function getBotChoice() {
     let randomChoice = Math.floor(Math.random() * 3) + 1;
     
     if (randomChoice === 1) {
@@ -20,32 +20,32 @@ function getComputerChoice() {
 }
 
 function playRound(e) {
-    const computerChoice = getComputerChoice();
+    const botChoice = getBotChoice();
     const playerChoice = e.target.innerText;
 
-    choiceDisplay.textContent = `You chose ${playerChoice} and the computer chose ${computerChoice}!`;
+    choiceDisplay.textContent = `You chose ${playerChoice} and the computer chose ${botChoice}!`;
 
-    if (playerChoice === computerChoice) {
+    if (playerChoice === botChoice) {
         results.textContent = `It's a DRAW!`; 
     } else if (playerChoice === "Rock") {
-        if (computerChoice === "Scissors") { // WIN rock beats scissors
+        if (botChoice === "Scissors") { // WIN rock beats scissors
             playerScore++;
             results.textContent = 'You WIN! Rock beats Scissors!';
         } else { // LOSE paper beats rock
-            computerScore++;
+            botScore++;
             results.textContent = 'You LOSE! Paper beats Rock!';
         }
     } else if (playerChoice === "Paper") {
-        if (computerChoice === "Rock") { // WIN paper beats rock
+        if (botChoice === "Rock") { // WIN paper beats rock
             playerScore++;
             results.textContent = 'You WIN! Paper beats Rock!';
         } else { // LOSE scissors beats paper
-            computerScore++;
+            botScore++;
             results.textContent = 'You LOSE! Scissors beats Paper!';
         }
     } else {
-        if (computerChoice === "Rock") { // LOSE rock beats scissors
-            computerScore++;
+        if (botChoice === "Rock") { // LOSE rock beats scissors
+            botScore++;
             results.textContent = 'You LOSE! Rock beats Scissors!'
         } else { // WIN scissors beats paper
             playerScore++;
@@ -54,15 +54,15 @@ function playRound(e) {
     }
 
     playerResult.textContent = playerScore;
-    computerResult.textContent = computerScore;
+    botResult.textContent = botScore;
 }
 
 function startGame(e) {
-    if (playerScore < 5 && computerScore < 5) playRound(e);
+    if (playerScore < 5 && botScore < 5) playRound(e);
     if (playerScore === 5) {
         results.textContent = 'YOU WIN THE GAME!';
         buttonPlayAgain.style.visibility = 'visible';
-    } else if (computerScore === 5) {
+    } else if (botScore === 5) {
         results.textContent = 'YOU LOSE THE GAME!';
         buttonPlayAgain.style.visibility = 'visible';
     }
@@ -70,14 +70,18 @@ function startGame(e) {
 
 function resetGame() {
     playerScore = 0;
-    computerScore = 0;
+    botScore = 0;
     playerResult.textContent = 0;
-    computerResult.textContent = 0;
+    botResult.textContent = 0;
     results.textContent = '';
     choiceDisplay.textContent = '';
 
     buttonPlayAgain.style.visibility = 'hidden';
 }
 
-buttonChoice.forEach(btn => btn.addEventListener('click', startGame));
+function displayChoice(player, bot) {
+    if (player === "Rock") icon.classList = 'fa-regular fa-hand';
+}
+
+playerOptions.forEach(btn => btn.addEventListener('click', startGame));
 buttonPlayAgain.addEventListener('click', resetGame)
